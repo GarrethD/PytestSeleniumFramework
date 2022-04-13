59f2e22bb2845970688afc13ee874961c624f8fc
GarrethD <54701909+GarrethD@users.noreply.github.com>
2022-04-13 23:32:47 +0200

    update to samplePageObjuects
    added new test case
    updated pageObjects
    Attempted to fix the waitForElement method

--- Test/PHPTravels.py
+++ Test/PHPTravels.py
@@ -1,5 +1,4 @@
 import unittest
-
 from selenium import webdriver
 from Base.Driver import Driver
 from PageObjects import SamplePageObjects
@@ -16,12 +15,18 @@ class TestExampleOne(unittest.TestCase):
         driver = Driver("CHROME")
         base_url = "https://www.phptravels.net/login"
 
-    def test_google_navigate(self):
+    def test_attempt_login(self):
         self.navigate_to_webpage()
+        self.enter_login_details()
 
     def navigate_to_webpage(self):
-        driver.navigate_to("https://www.phptravels.net/login")
-        print("Succesfully navigaated to " + self.base_url)
+        driver.navigate_to(base_url)
+
+    def enter_login_details(self):
+        driver.enter_text(SamplePageObjects.login_username_textfield(), "GarthD@gmail.com")
+        driver.enter_text(SamplePageObjects.login_password_textfield(), "NotMyPassword")
+        driver.click_element(SamplePageObjects.login_button())
+        driver.wait_element(SamplePageObjects.login_incorrect_user_details_error_message())
 
     def tearDown(self):
         driver.shut_down()
